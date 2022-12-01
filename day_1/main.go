6cf221245fa43777149407df51bbe65488fbac48
Dennis Wethmar <denniswethmar@gmail.com>
2022-12-01 17:06:40 +0100

    Nice

--- day_1/main.go
+++ day_1/main.go
@@ -8,15 +8,27 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
+func runningtime(s string) (string, time.Time) {
+	log.Println("Start:	", s)
+	return s, time.Now()
+}
+
+func track(s string, startTime time.Time) {
+	endTime := time.Now()
+	log.Printf("End: %s	%s", s, endTime.Sub(startTime))
+}
+
 func main() {
+	defer track(runningtime("get largest sum"))
+
 	var filePath string
 	if len(os.Args) < 1 {
-		fmt.Println("input : " + os.Args[0] + " file name")
+		fmt.Println("input : " + os.Args[0] + " not found")
 		os.Exit(1)
 	} else {
-		fmt.Println("input file name : " + os.Args[1])
 		filePath = os.Args[1]
 	}
 
@@ -26,20 +38,48 @@ func main() {
 	}
 	defer file.Close()
 
-	fmt.Println("largest sum : ", getLargestSum(file))
+	// fmt.Printf("Largest sums: %d\n", getLargestSums(file, 3))
+
+	sum := 0
+	for i, s := range getLargestSums(file, 3) {
+		fmt.Printf("Sum: %d %d\n", i, s)
+		sum += s
+	}
+
+	fmt.Printf("Sum of largest sums: %d\n", sum)
 }
 
-func getLargestSum(reader io.Reader) int {
+func getLargestSums(reader io.Reader, topN int) []int {
 	scanner := bufio.NewScanner(reader)
 
-	var sum, largestSum int
+	var sum int
+	largestSums := make([]int, topN)
 	counter := NewSummarizer(0)
 
-	for hasScan := scanner.Scan(); hasScan; hasScan = scanner.Scan() {
+	for scan := scanner.Scan(); scan; {
 		line := strings.TrimSuffix(scanner.Text(), "\n")
 
 		if line == "" {
 			counter = NewSummarizer(0)
+			// check if sum is larger than any of the largest sums
+			for i, largestSum := range largestSums {
+				if sum > largestSum {
+					// shift all smaller sums down
+					for j := topN - 1; j > i; j-- {
+						largestSums[j] = largestSums[j-1]
+					}
+
+					for j := len(largestSums) - 1; j > i; j-- {
+						largestSums[j] = largestSums[j-1]
+					}
+
+					largestSums[i] = sum
+					break
+				}
+			}
+
+			sum = 0
+			scan = scanner.Scan()
 			continue
 		}
 
@@ -48,16 +88,15 @@ func getLargestSum(reader io.Reader) int {
 			log.Fatal(err)
 		}
 
-		if sum = counter(i); sum > largestSum {
-			largestSum = sum
-		}
+		sum = counter(i)
+		scan = scanner.Scan()
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	return largestSum
+	return largestSums
 }
 
 func NewSummarizer(sum int) func(i int) int {
--- day_1/main_test.go
+++ day_1/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -9,37 +10,41 @@ import (
 func Test_getLargestSum(t *testing.T) {
 	type args struct {
 		reader io.Reader
+		topN   int
 	}
 	tests := []struct {
 		name string
 		args args
-		want int
+		want []int
 	}{
-		{
-			name: "test 1",
-			args: args{
-				reader: strings.NewReader("1\n-2\n3\n1\n\n1\n2\n3\n\n1"),
-			},
-			want: 6,
-		},
+		// {
+		// 	name: "test 1",
+		// 	args: args{
+		// 		reader: strings.NewReader(strings.Join([]string{"1", "2", "3", "", "2", "2", "3", "", "1", "2", "3"}, "\n")),
+		// 		topN:   1,
+		// 	},
+		// 	want: []int{7},
+		// },
 		{
 			name: "test 2",
 			args: args{
-				reader: strings.NewReader("1\n-2\n3\n1\n\n100"),
+				reader: strings.NewReader(strings.Join([]string{"1", "2", "3", "", "2", "2", "3", "", "100", "2", "3", "", "1"}, "\n")),
+				topN:   2,
 			},
-			want: 100,
+			want: []int{105, 7},
 		},
-		{
-			name: "test 3",
-			args: args{
-				reader: strings.NewReader("1\n-2\n3\n\n200\n\n1\n"),
-			},
-			want: 200,
-		},
+		// {
+		// 	name: "test 3",
+		// 	args: args{
+		// 		reader: strings.NewReader(""),
+		// 		topN:   3,
+		// 	},
+		// 	want: []int{0, 0, 0},
+		// },
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := getLargestSum(tt.args.reader); got != tt.want {
+			if got := getLargestSums(tt.args.reader, tt.args.topN); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("getLargestSum() = %v, want %v", got, tt.want)
 			}
 		})
