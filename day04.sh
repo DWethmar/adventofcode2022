c9a45d905dffa883c1248d483995031c45122de0
Dennis Wethmar <denniswethmar@gmail.com>
2022-12-04 11:04:04 +0100

    day 4 - part 2

--- day04.sh
+++ day04.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+echo 'test'
 go build -o $(pwd)/bin/day04 ./day04/*.go && bin/day04 $(pwd)/day04/input_test.txt 
 
+echo 'solution'
 go build -o $(pwd)/bin/day04 ./day04/*.go && bin/day04 $(pwd)/day04/input.txt 
--- day04/main.go
+++ day04/main.go
@@ -31,21 +31,40 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", p1)
+
+	file.Seek(0, 0)
+	p2, err := CountPartialOverlappingSectionAssignment(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Part 2: %d\n", p2)
+
 }
 
-func MapSections(i []string) (ax, ay, bx, by int) {
-	ax, _ = strconv.Atoi(i[0])
-	ay, _ = strconv.Atoi(i[1])
-	bx, _ = strconv.Atoi(i[2])
-	by, _ = strconv.Atoi(i[3])
+func MapSections(i []string) (x1, x2, y1, y2 int) {
+	x1, _ = strconv.Atoi(i[0])
+	x2, _ = strconv.Atoi(i[1])
+	y1, _ = strconv.Atoi(i[2])
+	y2, _ = strconv.Atoi(i[3])
 	return
 }
 
 func CountOverlappingSectionAssignment(input *os.File) (overlap int, err error) {
 	err = IterateLines(input, func(s string) {
-		var ax, ay, bx, by int = MapSections(re.FindAllString(s, -1))
+		var x1, x2, y1, y2 int = MapSections(re.FindAllString(s, -1))
 		// check if A contains B
-		if ax <= bx && ay >= by || bx <= ax && by >= ay {
+		if x1 <= y1 && x2 >= y2 || y1 <= x1 && y2 >= x2 {
+			overlap++
+		}
+	})
+	return
+}
+
+func CountPartialOverlappingSectionAssignment(input *os.File) (overlap int, err error) {
+	err = IterateLines(input, func(s string) {
+		var x1, x2, y1, y2 int = MapSections(re.FindAllString(s, -1))
+		// for any overlap
+		if x1 <= y2 && y1 <= x2 {
 			overlap++
 		}
 	})
